fb6516069fd45c781720c5e225c2c08c41d34c2c
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:38:09 +0500

    Add optimal solution for Find Closest Number To Zero

--- arrays-strings/optimal/FindClosestNumberToZeroOptimal.java
+++ arrays-strings/optimal/FindClosestNumberToZeroOptimal.java
@@ -1,1 +1,27 @@
+public class FindClosestNumberToZeroOptimal {
 
+    public static int findClosestNumber(int[] nums) {
+        int closest = nums[0];
+
+        for (int num : nums) {
+            if (Math.abs(num) < Math.abs(closest)) {
+                closest = num;
+            } else if (Math.abs(num) == Math.abs(closest) && num > closest) {
+                closest = num;
+            }
+        }
+
+        return closest;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -2, 1, 4, 8};
+
+        System.out.println("Closest Number to Zero: " + findClosestNumber(nums));
+    }
+}
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
