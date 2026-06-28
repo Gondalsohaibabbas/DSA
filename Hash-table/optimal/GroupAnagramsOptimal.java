5ca64470982ad09d96f9b85330b596d532bb90a0
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 23:02:17 +0500

    Add GroupAnagramsOptimal class for anagram grouping
    Implement an optimal solution to group anagrams using a HashMap.

---
+++ Hash-table/optimal/GroupAnagramsOptimal.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class GroupAnagramsOptimal {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+
+        System.out.println(groupAnagrams(strs));
+    }
+}
+
+/*
+Time Complexity: O(n × k log k)
+Space Complexity: O(n)
+*/
