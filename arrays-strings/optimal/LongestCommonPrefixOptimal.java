fa1f9db2d9a2a5481303a7312e3b709152ff820a
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:46:04 +0500

    Add optimal solution for Longest Common Prefix

--- arrays-strings/optimal/LongestCommonPrefixOptimal.java
+++ arrays-strings/optimal/LongestCommonPrefixOptimal.java
@@ -1,1 +1,36 @@
+import java.util.Arrays;
 
+public class LongestCommonPrefixOptimal {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0)
+            return "";
+
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        int i = 0;
+
+        while (i < first.length() && i < last.length()
+                && first.charAt(i) == last.charAt(i)) {
+            i++;
+        }
+
+        return first.substring(0, i);
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
+
+/*
+Time Complexity: O(n log n)
+Space Complexity: O(1)
+*/
