ab8a15a2cf9f73f630b3e4004ece3b796419e7d1
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:56:50 +0500

    Add brute force solution for containsDuplicate
    Implement brute force method to check for duplicates in an array.

--- Hash-table/brute/ContainsDuplicateBrute.java
+++ Hash-table/brute/ContainsDuplicateBrute.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
 
+public class ContainsDuplicateBrute {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
+
+/*
+Time Complexity: O(n log n)
+Space Complexity: O(1)
+*/
