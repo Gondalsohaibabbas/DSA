df8f362a8742ce3a6686b997a7b777bbc418a371
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 23:08:13 +0500

    Add optimal solution for Container With Most Water
    Implement an optimal solution to find the maximum area of water that can be contained.

---
+++ Two-pointers/optimal/ContainerWithMostWaterOptimal.java
@@ -0,0 +1,40 @@
+public class ContainerWithMostWaterOptimal {
+
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+
+            if (area > maxArea) {
+                maxArea = area;
+            }
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height));
+    }
+}
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
