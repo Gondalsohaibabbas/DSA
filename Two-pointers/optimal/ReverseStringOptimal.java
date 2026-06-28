72382dbcae7b3f42ad0efb2518c44b5b07d4f3ff
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 23:04:51 +0500

    Add ReverseStringOptimal class to reverse a string
    Implement an optimal algorithm to reverse a character array in place using two pointers.

---
+++ Two-pointers/optimal/ReverseStringOptimal.java
@@ -0,0 +1,32 @@
+public class ReverseStringOptimal {
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h','e','l','l','o'};
+
+        reverseString(s);
+
+        System.out.println(s);
+    }
+}
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
