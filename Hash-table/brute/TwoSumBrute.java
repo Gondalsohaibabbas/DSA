d30bcb23d5b54a486f1733e2066621a9d7c96da8
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:58:11 +0500

    Add TwoSumBrute class with brute force algorithm
    Implement brute force solution for Two Sum problem.

---
+++ Hash-table/brute/TwoSumBrute.java
@@ -0,0 +1,32 @@
+public class TwoSumBrute {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
+
+/*
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
