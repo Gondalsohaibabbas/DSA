1e254dcba13a431229e238a55d140123eddafa1a
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:57:24 +0500

    Add containsDuplicate method using HashSet
    Implemented a method to check for duplicates in an array using a HashSet for optimal performance.

--- Hash-table/optimal/ContainsDuplicateOptimal.java
+++ Hash-table/optimal/ContainsDuplicateOptimal.java
@@ -1,1 +1,32 @@
+import java.util.HashSet;
 
+public class ContainsDuplicateOptimal {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
