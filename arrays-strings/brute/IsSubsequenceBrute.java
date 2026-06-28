5c7938129d4b49bad016157704bb02a2f30e3c5a
Sohaib Abbas <sohaibabbas660@gmail.com>
2026-06-28 22:40:01 +0500

    Add brute force solution for Is Subsequence

--- arrays-strings/brute/IsSubsequenceBrute.java
+++ arrays-strings/brute/IsSubsequenceBrute.java
@@ -1,1 +1,28 @@
+public class IsSubsequenceBrute {
 
+    public static boolean isSubsequence(String s, String t) {
+
+        int i = 0;
+
+        for (int j = 0; j < t.length() && i < s.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abc";
+        String t = "ahbgdc";
+
+        System.out.println(isSubsequence(s, t));
+    }
+}
+
+/*
+Time Complexity: O(n × m)
+Space Complexity: O(1)
+*/
